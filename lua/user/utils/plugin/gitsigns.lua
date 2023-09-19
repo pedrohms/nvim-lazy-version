17e3fd7ccf1cac52a3906b9db8711b612debaa51
Pedro Souza <pedrohms412@gmail.com>
2023-09-19 18:40:30 -0300

    Undoing gitsigns configurations

--- lua/user/utils/plugin/gitsigns.lua
+++ lua/user/utils/plugin/gitsigns.lua
@@ -12,23 +12,23 @@ gitsigns.setup {
     changedelete = { hl = "GitSignsChange", text = "▎", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
   },
   signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-  numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
-  linehl = true, -- Toggle with `:Gitsigns toggle_linehl`
-  word_diff = true, -- Toggle with `:Gitsigns toggle_word_diff`
+  numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
+  linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
+  word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
   watch_gitdir = {
-    interval = 200,
+    interval = 1000,
     follow_files = true,
   },
   attach_to_untracked = true,
-  current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+  current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
   current_line_blame_opts = {
-    virt_text = true,
+    virt_text = false,
     virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-    delay = 200,
+    delay = 1000,
     ignore_whitespace = false,
   },
   current_line_blame_formatter_opts = {
-    relative_time = true,
+    relative_time = false,
   },
   sign_priority = 6,
   update_debounce = 100,
